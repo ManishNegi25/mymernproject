d466daee85cfe94a77f798785c0e712d7b3d1fa4
MANISHNEGI <147482023+ManishNegi25@users.noreply.github.com>
2024-04-14 23:50:32 +0530

    change config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mymernproject.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+};
